2034ba3f72cb71b8b16699a2ee7060645b7a5899
Artnalyze <artnalyze@gmail.com>
2021-12-06 15:35:59 +0700

    set route and link bootstrap css , js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom'
+import {routes} from './routes'
 
 ReactDOM.render(
   <React.StrictMode>
-
+    <Router>
+      { routes }
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
+
+const routes = [
+    <Routes>
+        {/* Frontend */}
+        {/*
+        <Route path="/" exect={true} element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        */}
+        {/* Backend */}
+        {/* <Route path="/" exect={true} element={} /> */}
+    </Routes>
+]
+
+export default routes
